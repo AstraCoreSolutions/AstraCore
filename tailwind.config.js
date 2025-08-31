2fc9880bafeafa747ad4b1e981accc349d34cbcd
AstraCoreSolutions <info@astracore-solutions.cz>
2025-08-31 14:08:34 +0200

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'astra-orange': {
+          400: '#F56500',
+          500: '#ED8936',
+          600: '#DD6B20'
+        }
+      },
+      fontFamily: {
+        sans: ['-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Helvetica Neue', 'Arial', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
